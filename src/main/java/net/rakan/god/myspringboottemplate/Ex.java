dd662f632b3c7a5db7477280d4488faf2fcdc3cd
as-luotianshen <1924097082@qq.com>
2023-08-01 09:16:01 +0800

    初始化源码

--- src/main/java/net/rakan/god/myspringboottemplate/Ex.java
+++ src/main/java/net/rakan/god/myspringboottemplate/Ex.java
@@ -1,4 +1,17 @@
 package net.rakan.god.myspringboottemplate;
 
+import java.math.BigDecimal;
+
 public class Ex {
+    public static void main(String[] args) {
+        BigDecimal b1 = new BigDecimal(1);
+        BigDecimal b2 = new BigDecimal(1.0);
+        int scale = b2.scale();
+        int scale1 = b1.scale();
+
+        System.out.println(scale1);
+        System.out.println(scale);
+
+        System.out.println(b1.equals(b2));
+    }
 }
