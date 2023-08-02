0f37db1cb4bcfea6ff84ee119072e6536c0bb680
as-luotianshen <1924097082@qq.com>
2023-08-02 09:49:12 +0800

    初始化源码

--- src/main/java/net/rakan/god/myspringboottemplate/Ex.java
+++ src/main/java/net/rakan/god/myspringboottemplate/Ex.java
@@ -4,14 +4,17 @@ import java.math.BigDecimal;
 
 public class Ex {
     public static void main(String[] args) {
-        BigDecimal b1 = new BigDecimal(1);
-        BigDecimal b2 = new BigDecimal(1.0);
-        int scale = b2.scale();
-        int scale1 = b1.scale();
-
-        System.out.println(scale1);
-        System.out.println(scale);
-
-        System.out.println(b1.equals(b2));
+//        BigDecimal b1 = new BigDecimal(1);
+//        BigDecimal b2 = new BigDecimal(1.0);
+//        int scale = b2.scale();
+//        int scale1 = b1.scale();
+//
+//        System.out.println(scale1);
+//        System.out.println(scale);
+//
+//        System.out.println(b1.equals(b2));
+//        String s = "9577ef80-d328-5780-282b-37b6ac860dbd";
+        String s = "9577ef80-d328-5780-282b-37b6ac860dbd";
+        System.out.println(s.hashCode());
     }
 }
