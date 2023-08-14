363bbc568b4297c6804e6797c1723de253870298
as-luotianshen <1924097082@qq.com>
2023-08-14 17:47:19 +0800

    初始化源码

--- src/main/java/net/rakan/god/myspringboottemplate/common/service/NettyService.java
+++ src/main/java/net/rakan/god/myspringboottemplate/common/service/NettyService.java
@@ -1,38 +1,126 @@
 package net.rakan.god.myspringboottemplate.common.service;
 
+import java.net.InetSocketAddress;
+
+import net.rakan.god.myspringboottemplate.common.handler.WebsocketMessageHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.stereotype.Component;
+
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.buffer.PooledByteBufAllocator;
-import io.netty.channel.ChannelFuture;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.protobuf.ProtobufDecoder;
-import io.netty.handler.codec.protobuf.ProtobufEncoder;
-import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
-import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
-import io.netty.handler.logging.LogLevel;
-import io.netty.handler.logging.LoggingHandler;
-import io.netty.handler.timeout.IdleStateHandler;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketServerCompressionHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
 
 /**
- * 启动netty服务(利用ApplicationListener上下文监听器)
+ * 初始化Netty服务
  *
- * @Author LiChangRui on 2023/8/2 11:29
+ * @Author LiChangRui on 2023/8/14 17:31
  */
 @Component
-public class NettyService implements ApplicationListener<ContextRefreshedEvent> {
+public class NettyService implements ApplicationRunner, ApplicationListener<ContextClosedEvent>, ApplicationContextAware {
 
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
+    private static final Logger LOGGER = LoggerFactory.getLogger(NettyService.class);
 
+    @Value("${netty.websocket.port}")
+    private int port;
+
+    @Value("${netty.websocket.ip}")
+    private String ip;
+
+    @Value("${netty.websocket.path}")
+    private String path;
+
+    @Value("${netty.websocket.max-frame-size}")
+    private long maxFrameSize;
+
+    private ApplicationContext applicationContext;
+
+    private Channel serverChannel;
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    public void run(ApplicationArguments args) throws Exception {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup);
+            serverBootstrap.channel(NioServerSocketChannel.class);
+            serverBootstrap.localAddress(new InetSocketAddress(this.ip, this.port));
+            serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel socketChannel) throws Exception {
+                    ChannelPipeline pipeline = socketChannel.pipeline();
+                    pipeline.addLast(new HttpServerCodec());
+                    pipeline.addLast(new ChunkedWriteHandler());
+                    pipeline.addLast(new HttpObjectAggregator(65536));
+                    pipeline.addLast(new ChannelInboundHandlerAdapter() {
+                        @Override
+                        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                            if (msg instanceof FullHttpRequest) {
+                                FullHttpRequest fullHttpRequest = (FullHttpRequest) msg;
+                                String uri = fullHttpRequest.uri();
+                                if (!uri.equals(path)) {
+                                    // 访问的路径不是 websocket的端点地址，响应404
+                                    ctx.channel().writeAndFlush(new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.NOT_FOUND))
+                                            .addListener(ChannelFutureListener.CLOSE);
+                                    return;
+                                }
+                            }
+                            super.channelRead(ctx, msg);
+                        }
+                    });
+                    pipeline.addLast(new WebSocketServerCompressionHandler());
+                    pipeline.addLast(new WebSocketServerProtocolHandler(path, null, true, maxFrameSize));
+
+                    /**
+                     * 从IOC中获取到Handler
+                     */
+                    pipeline.addLast(applicationContext.getBean(WebsocketMessageHandler.class));
+                }
+            });
+            Channel channel = serverBootstrap.bind().sync().channel();
+            this.serverChannel = channel;
+            LOGGER.info("websocket 服务启动，ip={},port={}", this.ip, this.port);
+            channel.closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public void onApplicationEvent(ContextClosedEvent event) {
+        if (this.serverChannel != null) {
+            this.serverChannel.close();
+        }
+        LOGGER.info("websocket 服务停止");
     }
 }
